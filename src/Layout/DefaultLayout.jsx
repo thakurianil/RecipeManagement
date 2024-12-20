374f78ecc1b8ba3dccab6861ea8756aeec8e628f
Anil khand <anilkhandthakuri@gmail.com>
2024-12-20 23:21:53 +1100

    done

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -10,14 +10,7 @@ export const DefaultLayout = ({ children, pageTitle }) => {
     
     <div>
 
-      <Container>
-        <div className="p-2">{pageTitle}</div>
-        <main className="main">
-          <Outlet />
-        </main>
-      </Container>
 
-      <Footer />
     </div>
   );
 };
